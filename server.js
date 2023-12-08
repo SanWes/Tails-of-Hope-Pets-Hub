8bba671db372740c210cd01df6b6a572b231dc2c
Wesley Sanchez <77991913+SanWes@users.noreply.github.com>
2023-12-08 15:50:56 -0500

    Updates to Server Side

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const cors = require("cors")
+require('dotenv').config();
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
-app.use( express.json() ); //tells my app that it can parse json
-app.use( express.urlencoded({ extended: true }) ); //tells my app that it can gather form information
-app.use(cors()); //tells the app that it is allowed to share resources  
+app.use( express.json() ); // tells my app that it can parse json
+app.use( express.urlencoded({ extended: true }) ); // tells my app that it can gather form information
+app.use(cors()); // tells the app that it is allowed to share resources  
 
 require("./server/config/config");
 
-
-//require the routes
+// require the routes
 require("./server/routes/petShelter.routes")(app)
 
 
--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,46 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 require('dotenv').config();
-const db_name = process.env.db_name;
+const { SECRET, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, MONGODB_URI } = process.env;
+
+// console.log(MONGODB_URI, typeof MONGODB_URI);
+
+mongoose.set('strictQuery', false);
 
 //mongoose connection/config
-mongoose.connect(`mongodb+srv://root:root@mern.6etyz.mongodb.net/${db_name}?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log(`Established a connection to the ${db_name} database`))
-    
-    .catch(err => console.log(`Something went wrong when connecting to the database: ${db_name} `, err));
+mongoose.connect( MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connection to Database Success!"))
+    .catch(err => console.log( err));
+
+
+
+
+
+    // mongoose.connect( uri, {useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log("Connection to Database Success!"))
+//     .catch(err => console.log( err));
+
+
+// Decrypting Scripts
+function decrypt(encryptedData, inputIV) {
+    try {
+        const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), Buffer.from(inputIV, 'hex'));
+        let decrypted = decipher.update(encryptedData, 'hex', 'utf-8');
+        decrypted += decipher.final('utf-8');
+        return decrypted;
+    } catch (error) {
+        // Handle any potential errors during decryption
+        console.error('Decryption error:', error);
+        return null; // Or handle the error appropriately
+    }
+};
+
+// const algorithm = 'aes-256-cbc';
+// const key = crypto.scryptSync(process.env.DB_PASSWORD, 'salt', 32);
+// const iv = crypto.randomBytes(16);
+
+// const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), Buffer.from(inputIV, 'hex'))
+// let decrypted = decipher.update(process.env.MONGODB_URI, 'hex', 'utf-8');
+// decrypted += decipher.final('utf-8');
+
+// const uri = decrypt(MONGODB_URI, iv);
